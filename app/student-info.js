6ffa2f0c9f35445f9f5eb3685b3fbbef708b74d1
sumaiyakd <sumaiya.khurshid@edu.sait.ca>
2024-09-15 18:16:21 -0600

    week 2 assignmnet
    week 2 assignment done

---
+++ app/student-info.js
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import React from 'react';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Sumaiya Khurshid</h1>
+      <p>
+        <Link href="https://github.com/sumaiyakd/cprg306-assignments" target="_blank" rel="noopener noreferrer">
+        https://github.com/sumaiyakd/cprg306-assignments
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default StudentInfo;
