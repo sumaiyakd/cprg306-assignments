09ba702af9c365b23500a128fec4718366d1a774
sumaiyakd <145646995+sumaiyakd@users.noreply.github.com>
2024-10-28 20:03:36 -0600

    Update page.js

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -1,12 +1,15 @@
+// page.js
 import React from 'react';
 import ItemList from './item-list';
 
 const Page = () => {
   return (
-    <main className="min-h-screen bg-gray-900 p-8">
-      <h1 className="text-3xl font-bold text-center text-white mb-8">Shopping List</h1>
-      <ItemList />
-    </main>
+      <main className="bg-gray-900 min-h-screen p-5">
+          <div className="w-full max-w-lg">
+              <h1 className="text-white text-3xl font-bold mb-5">Shopping List</h1>
+              <ItemList />
+          </div>
+      </main>
   );
 };
 
